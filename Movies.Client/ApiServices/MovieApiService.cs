829bca2b1438f191bd33453baa5dcee5ba6ddfd4
Asit Kumar Mohanty <asit199@gmail.com>
2021-06-22 10:30:10 +0530

    Movies Client Authentication through HttpHandler

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
 using Movies.Client.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +12,15 @@ namespace Movies.Client.ApiServices
 {
     public class MovieApiService : IMovieApiService
     {
-        //private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieApiService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         //private readonly IHttpContextAccessor _httpContextAccessor;
+
 
         //public MovieApiService(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
         //{
@@ -22,20 +31,87 @@ namespace Movies.Client.ApiServices
 
         public async Task<IEnumerable<Movie>> GetMovies()
         {
-            var movieList = new List<Movie>();
-            movieList.Add(
-                new Movie
-                {
-                    Id = 1,
-                    Genre = "Thriller",
-                    Title = "Chernobyl",
-                    Rating = "9.5",
-                    ImageUrl = "image/src",
-                    ReleaseDate = DateTime.Now,
-                    Owner = "HBO"
-                }
-                );
+            // WAY 1
+
+            //1st create a request getting it from Startup.cs
+            var httpClient = _httpClientFactory.CreateClient("MovieAPIClient");
+
+            //This is how we should prepare the request url
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/movies/");
+
+            // send the request
+            var response = await httpClient.SendAsync(
+                request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
             return movieList;
+
+            //// WAY 2
+            // 1- Get token from Identity server , 
+            // 2 - Send request to Protected Api
+            // 3 - Deserialize object to MovieList
+
+            //// 1. "retrieve" our api credentials. This must be registered on Identity Server!
+            //var apiClientCredentials = new ClientCredentialsTokenRequest
+            //{
+            //    Address = "https://localhost:5005/connect/token",
+
+            //    ClientId = "movieClient",
+            //    ClientSecret = "secret",
+
+            //    // This is the scope our Protected API requires. 
+            //    Scope = "movieAPI"
+            //};
+
+            //// creates a new httpclient to talk to our identityserver (localhost:5005)
+            //var client = new HttpClient();
+
+            //// just checks if we can reach the Discovery document. Not 100% needed but..
+            //var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            //if (disco.IsError)
+            //{
+            //    return null; // throw 500 error
+            //}
+
+            //// 2. Authenticates and get an access token from Identity Server
+            //var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            //if (tokenResponse.IsError)
+            //{
+            //    return null;
+            //}
+
+            //// Another HttpClient for talking now with our Protected API
+            //var apiClient = new HttpClient();
+
+            //// 3. Set the access_token in the request Authorization: Bearer <token>
+            //apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            //// 4. Send a request to our Protected API
+            //var response = await apiClient.GetAsync("https://localhost:5001/api/movies");
+            //response.EnsureSuccessStatusCode();
+
+            //var content = await response.Content.ReadAsStringAsync();
+
+            //var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            //return movieList;
+
+            //var movieList = new List<Movie>();
+            //movieList.Add(
+            //    new Movie
+            //    {
+            //        Id = 1,
+            //        Genre = "Thriller",
+            //        Title = "Chernobyl",
+            //        Rating = "9.5",
+            //        ImageUrl = "image/src",
+            //        ReleaseDate = DateTime.Now,
+            //        Owner = "HBO"
+            //    }
+            //    );
+            //return movieList;
         }
         public Task<Movie> CreateMovie(Movie movie)
         {
--- Movies.Client/Startup.cs
+++ Movies.Client/Startup.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
 using Movies.Client.ApiServices;
+using Movies.Client.HttpHandlers;
+using System;
 
 namespace Movies.Client
 {
@@ -29,39 +33,69 @@ namespace Movies.Client
             {
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            })
-                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
-                {
-                    options.Authority = "https://localhost:5005";
+            }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.Authority = "https://localhost:5005";
 
-                    options.ClientId = "movies_mvc_client";
-                    options.ClientSecret = "secret";
-                    options.ResponseType = "code";
+                options.ClientId = "movies_mvc_client";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
 
-                    options.Scope.Add("openid");
-                    options.Scope.Add("profile");
-                    //options.Scope.Add("address");
-                    //options.Scope.Add("email");
-                    //options.Scope.Add("roles");
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                //options.Scope.Add("address");
+                //options.Scope.Add("email");
+                //options.Scope.Add("roles");
 
-                    //options.ClaimActions.DeleteClaim("sid");
-                    //options.ClaimActions.DeleteClaim("idp");
-                    //options.ClaimActions.DeleteClaim("s_hash");
-                    //options.ClaimActions.DeleteClaim("auth_time");
-                    //options.ClaimActions.MapUniqueJsonKey("role", "role");
+                //options.ClaimActions.DeleteClaim("sid");
+                //options.ClaimActions.DeleteClaim("idp");
+                //options.ClaimActions.DeleteClaim("s_hash");
+                //options.ClaimActions.DeleteClaim("auth_time");
+                //options.ClaimActions.MapUniqueJsonKey("role", "role");
 
-                    //options.Scope.Add("movieAPI");
+                //options.Scope.Add("movieAPI");
 
-                    options.SaveTokens = true;
-                    options.GetClaimsFromUserInfoEndpoint = true;
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
 
-                    //options.TokenValidationParameters = new TokenValidationParameters
-                    //{
-                    //    NameClaimType = JwtClaimTypes.GivenName,
-                    //    RoleClaimType = JwtClaimTypes.Role
-                    //};
-                });
+                //options.TokenValidationParameters = new TokenValidationParameters
+                //{
+                //    NameClaimType = JwtClaimTypes.GivenName,
+                //    RoleClaimType = JwtClaimTypes.Role
+                //};
+            });
+
+            // http operations
+
+            // 1 create an HttpClient used for accessing the Movies.API
+            services.AddTransient<AuthenticationDelegatingHandler>();
+
+            services.AddHttpClient("MovieAPIClient", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/"); 
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+            }).AddHttpMessageHandler<AuthenticationDelegatingHandler>();
+
+            // 2 create an HttpClient used for accessing the IDP
+            // This is for Identity server
+            services.AddHttpClient("IDPClient", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5005/");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+            });
+
+            services.AddSingleton(new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:5005/connect/token",
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+                Scope = "movieAPI"
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
